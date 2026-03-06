0148c104269923e5c13a82839235c6405990f5cd
TioElvis <elvis.veramijares@gmail.com>
2026-03-06 22:25:53 +0100

    api: implement jwt module with passport for authentication

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AppController } from './app.controller';
@@ -17,6 +19,15 @@ import { AppController } from './app.controller';
         dbName: config.get<string>('DB_NAME'),
       }),
     }),
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '7d' },
+      }),
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
   controllers: [AppController],
 })
