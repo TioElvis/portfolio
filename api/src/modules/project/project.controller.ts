de92aeb8bf02545a6ce0a3e0cd478d025b8d4987
TioElvis <elvis.veramijares@gmail.com>
2026-03-07 16:46:40 +0100

    api: implement create project endpoint

--- api/src/modules/project/project.controller.ts
+++ api/src/modules/project/project.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+
+import { CreateProjectDto } from './dto/create-project.dto';
 
 import { ProjectService } from './project.service';
 
 @Controller('project')
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
+
+  @Post('create')
+  @UseGuards(AuthGuard('jwt'))
+  async create(@Body() body: CreateProjectDto) {
+    return await this.projectService.create(body);
+  }
 }
--- api/src/modules/project/project.service.ts
+++ api/src/modules/project/project.service.ts
@@ -1,8 +1,15 @@
+import {
+  BadRequestException,
+  forwardRef,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
 
 import { Project, ProjectDocument } from './project.schema';
+
+import { CreateProjectDto } from './dto/create-project.dto';
 
 import { SectionService } from 'src/modules/section/section.service';
 
@@ -13,4 +20,25 @@ export class ProjectService {
     @Inject(forwardRef(() => SectionService))
     private sectionService: SectionService,
   ) {}
+
+  async create(body: CreateProjectDto) {
+    const existingProject = await this.projectModel
+      .findOne({ slug: body.slug })
+      .exec();
+
+    if (existingProject) {
+      throw new BadRequestException('Project with this slug already exists');
+    }
+
+    const payload: Project = { ...body };
+
+    try {
+      const project = await this.projectModel.create(payload);
+
+      return { message: 'Project created successfully', data: project };
+    } catch (error) {
+      console.error('Error creating project:', error);
+      throw new BadRequestException('Failed to create project');
+    }
+  }
 }
