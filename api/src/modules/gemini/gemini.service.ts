cb451897b3f9050c99768b39c4a2d5d6528efd53
TioElvis <elvis.veramijares@gmail.com>
2026-03-14 19:46:16 +0100

    api: implement GeminiService with GoogleGenAI integration and configuration

--- api/src/modules/gemini/gemini.service.ts
+++ api/src/modules/gemini/gemini.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { GoogleGenAI } from '@google/genai';
+import { ConfigService } from '@nestjs/config';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class GeminiService {}
+export class GeminiService implements OnModuleInit {
+  private genAI: GoogleGenAI;
+  private model: string;
+
+  constructor(private configService: ConfigService) {}
+
+  onModuleInit() {
+    this.genAI = new GoogleGenAI({
+      apiKey: this.configService.get<string>('GEMINI_KEY'),
+    });
+    this.model = this.configService.get<string>('GEMINI_MODEL')!;
+  }
+}
