1a962ce41d7760641f04d0206a116b4644cade97
TioElvis <elvis.veramijares@gmail.com>
2026-03-14 19:15:18 +0100

    api: export GithubService from GithubModule for external usage

--- api/src/modules/github/github.module.ts
+++ api/src/modules/github/github.module.ts
@@ -6,5 +6,6 @@ import { GithubController } from './github.controller';
 @Module({
   controllers: [GithubController],
   providers: [GithubService],
+  exports: [GithubService],
 })
 export class GithubModule {}
