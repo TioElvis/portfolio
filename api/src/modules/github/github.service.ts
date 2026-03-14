15d02c1eb18240b76b0992e0da2f47aff1dd0796
TioElvis <elvis.veramijares@gmail.com>
2026-03-14 16:53:10 +0100

    api: implement GithubService with Octokit integration and configuration

--- api/src/modules/github/github.service.ts
+++ api/src/modules/github/github.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Octokit } from '@octokit/rest';
+import { ConfigService } from '@nestjs/config';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class GithubService {}
+export class GithubService implements OnModuleInit {
+  private octokit: Octokit;
+
+  constructor(private configService: ConfigService) {}
+
+  onModuleInit() {
+    this.octokit = new Octokit({
+      auth: this.configService.get<string>('GITHUB_TOKEN'),
+    });
+  }
+}
